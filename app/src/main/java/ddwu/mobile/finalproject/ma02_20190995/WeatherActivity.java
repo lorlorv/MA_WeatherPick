5906a7ff9921e0500715b38722a0b1143fb2d3ac
이은서 <leuns36@naver.com>
2021-12-16 00:21:49 +0900

    Feat : WeatherActivity 생성

--- app/src/main/java/ddwu/mobile/finalproject/ma02_20190995/WeatherActivity.java
+++ app/src/main/java/ddwu/mobile/finalproject/ma02_20190995/WeatherActivity.java
@@ -1,4 +1,12 @@
 package ddwu.mobile.finalproject.ma02_20190995;
 
-public class WeatherActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
